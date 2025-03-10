cd1d9c4f75773db8c06c2d659b827ead1bc92715
Naoto Hamada <naoto.hamada@findy.co.jp>
2025-03-11 02:38:02 +0900

    Add factory-bot

--- engines/feature_user/lib/feature_user/engine.rb
+++ engines/feature_user/lib/feature_user/engine.rb
@@ -1,5 +1,16 @@
 module FeatureUser
   class Engine < ::Rails::Engine
     isolate_namespace FeatureUser
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
