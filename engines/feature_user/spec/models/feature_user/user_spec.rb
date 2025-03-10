0f43192720ffea896e1f7f5a10482bccf4fdb2b0
Naoto Hamada <naoto.hamada@findy.co.jp>
2025-03-11 02:18:25 +0900

    Add mariadb-client

---
+++ engines/feature_user/spec/models/feature_user/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+module FeatureUser
+  RSpec.describe User, type: :model do
+    it 'is valid with a name' do
+      user = User.create(name: 'test')
+      expect(user).to be_valid
+    end
+  end
+end
