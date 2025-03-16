a3a14dd27e34b2954e23f640d72b4998982d818e
Naoto Hamada <naoto.hamada@findy.co.jp>
2025-03-16 14:18:44 +0900

    Fix brakeman and rspec

--- engines/feature_user/spec/models/feature_user/user_spec.rb
+++ engines/feature_user/spec/models/feature_user/user_spec.rb
@@ -6,7 +6,7 @@ module FeatureUser
   RSpec.describe User, type: :model do
     it 'is valid with a name' do
       user = User.create(name: 'test')
-      expect(user).to be_invalid
+      expect(user).to be_valid
     end
   end
 end
